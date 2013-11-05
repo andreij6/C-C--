c3b1256d03516e15bdfe280720a8e478948837e2
Andre Jones <andreij6@gmail.com>
2013-11-05 13:23:05 -0600

    first commit
    k

---
+++ Exercise Files/Chap02/workingDONE.c
@@ -0,0 +1,8 @@
+// working.c by Bill Weinman <http://bw.org/>
+#include <stdio.h>
+
+int main( int argc, char ** argv ) {
+	fprintf(stdout, "Hello, World!\n"); //stdout = standard output
+	fprintf(stderr, "Hello, World!\n"); //standard error
+	return 0;
+}
---
+++ Exercise Files/Chap02/workingDONE.cpp
@@ -0,0 +1,9 @@
+// working.cpp by Bill Weinman <http://bw.org/>
+#include <iostream>
+//using namespace std;  when this is commented out use std:: namespaces
+
+int main( int argc, char ** argv ) {
+	std::cout << "Hello, World!" << std::endl;  //cout = stdout in C  endl is equivelent to \n
+	std::cerr << "Hello, World!\n";  //c error = stderr in C
+	return 0;
+}
